7b0c61a1e94955dc6db98b66808c4a4e88657054
Trommik <Schwarkthomas@outlook.de>
2017-01-04 14:16:54 +0100

    Add files via upload

---
+++ Charliecube_Dateien/SpireTest/SpireTest.ino
@@ -0,0 +1,80 @@
+//CharlieCube Spire Test Program
+
+#define PIN_A 3
+#define PIN_B 4
+#define PIN_C 6
+#define PIN_D 5
+int delayTime = 300;
+
+void setup()
+{
+// first set all pins to input, or high impedance
+// (not strictly necessary as all pins are inputs by default)
+pinMode(PIN_A, INPUT);
+pinMode(PIN_B, INPUT);
+pinMode(PIN_C, INPUT);
+pinMode(PIN_C, INPUT);
+}
+
+void loop()
+{
+
+set_pins(PIN_B, PIN_A);
+delay(delayTime);
+set_pins(PIN_C, PIN_A);
+delay(delayTime);
+set_pins(PIN_D, PIN_A);
+delay(delayTime);
+
+set_pins(PIN_C, PIN_B);
+delay(delayTime);
+set_pins(PIN_D, PIN_B);
+delay(delayTime);
+set_pins(PIN_A, PIN_B);
+delay(delayTime);
+
+set_pins(PIN_D, PIN_C);
+delay(delayTime);
+set_pins(PIN_A, PIN_C);
+delay(delayTime);
+set_pins(PIN_B, PIN_C);
+delay(delayTime);
+
+set_pins(PIN_A, PIN_D);
+delay(delayTime);
+set_pins(PIN_B, PIN_D);
+delay(delayTime);
+set_pins(PIN_C, PIN_D);
+delay(delayTime);
+
+
+
+}
+
+void set_pins(int low_pin, int high_pin)
+{
+// reset all the pins
+reset_pins();
+
+// set the high and low pins to output
+pinMode(high_pin, OUTPUT);
+pinMode(low_pin, OUTPUT);
+
+// set high pin to logic high, low to logic low
+digitalWrite(high_pin, HIGH);
+digitalWrite(low_pin,LOW);
+}
+
+void reset_pins()
+{
+// start by ensuring all pins are at input and low
+pinMode(PIN_A, INPUT);
+pinMode(PIN_B, INPUT);
+pinMode(PIN_C, INPUT);
+pinMode(PIN_D, INPUT);
+
+digitalWrite(PIN_A, LOW);
+digitalWrite(PIN_B, LOW);
+digitalWrite(PIN_C, LOW);
+digitalWrite(PIN_D, LOW);
+}
---
+++ Charliecube_Dateien/charlieCubeV2/charlieCubeV2.ino
@@ -0,0 +1,272 @@
+#include "sphereplex.h"
+
+int color = green;
+
+int animationSpeed = 40;
+
+const int input[4] = { 14, 15, 16, 17};
+
+void setup() {
+  initCube();
+  // how many secconds until the animation is told to progress
+  animationMax = 10;
+ 
+}
+void loop() {
+  /*
+  if(analogRead(input[0])==0){
+    for(int i = 0; i < 6; i++){
+      kreisel(20 * i + 30);
+      if(i < 3){
+        kreisel(20 * i + 30);
+      }
+    }
+    for(int i = 6; i > 0; i--){
+      kreisel(20 * i + 30);
+      if(i < 3){
+        kreisel(20 * i + 30);
+      }
+    }
+    color = nextColor(color);
+    
+  }else if(analogRead(input[1])==0){
+  //planarFlop3D();
+    trifade();
+    
+  }else if(analogRead(input[2])==0){
+    fountian();
+    
+  }else if(analogRead(input[3])==0){
+    chaseTheDot();
+    
+  }*/
+  drawLed(2,0,0,0);
+    flushBuffer();
+    clearBuffer();
+    delay(animationSpeed);
+}
+
+
+void kreisel(int animationSpeed) {
+  
+    drawLine(color, 0, 0, 0, 0, 0, 2);     
+    flushBuffer();
+    clearBuffer();
+    delay(animationSpeed);  
+
+    drawLine(color, 1, 0, 0, 1, 0, 2);     
+    flushBuffer();
+    clearBuffer();
+    delay(animationSpeed); 
+
+    drawLine(color, 2, 0, 0, 2, 0, 2);     
+    flushBuffer();
+    clearBuffer();
+    delay(animationSpeed);  
+
+    drawLine(color, 2, 1, 0, 2, 1, 2);     
+    flushBuffer();
+    clearBuffer();
+    delay(animationSpeed); 
+
+    drawLine(color, 2, 2, 0, 2, 2, 2);     
+    flushBuffer();
+    clearBuffer();
+    delay(animationSpeed); 
+
+    drawLine(color, 1, 2, 0, 1, 2, 2);     
+    flushBuffer();
+    clearBuffer();
+    delay(animationSpeed); 
+
+    drawLine(color, 0, 2, 0, 0, 2, 2);     
+    flushBuffer();
+    clearBuffer();
+    delay(animationSpeed);  
+
+    drawLine(color, 0, 1, 0, 0, 1, 2);     
+    flushBuffer();
+    clearBuffer();
+    delay(animationSpeed);  
+}
+
+void fountian() {
+  continuePattern = true;
+  int animationSpeed = 150;
+  while (continuePattern) {
+    for (int z = 0; z <= 2; z++) {
+      drawLed(color, 1, 1, z); 
+      //drawBoxWalls(color,1,1,z,2,2,z);
+      flushBuffer();
+      clearBuffer();
+      delay(animationSpeed-30);
+    }
+    for (int z = 2; z >= 0; z--) {
+      
+      drawBoxWalls(color,0,0,z,2,2,z);
+      flushBuffer();
+      clearBuffer();
+      delay(animationSpeed);
+    }
+    color=nextColor(color);
+  }
+}
+
+void chaseTheDot() {
+  continuePattern = true;
+  int animationSpeed = 100;
+  
+  int xpos = 0;
+  int ypos = 0;
+  int zpos = 0;
+  
+  while (continuePattern) {
+    switch(random(0,6)) {
+      case 0:
+        if (xpos > 0) {xpos--;break;}
+        else color=nextColor(color);
+      case 1:
+        if (xpos < 2) {xpos++;break;}
+        else color=nextColor(color);
+        xpos--; break;
+        
+      case 2:
+        if (ypos > 0) {ypos--;break;}
+        else color=nextColor(color);
+      case 3:
+        if (ypos < 2) {ypos++;break;}
+        else color=nextColor(color);
+        ypos--; break;
+      
+      case 4:
+        if (zpos > 0) {zpos--;break;}
+        else color=nextColor(color);
+      case 5:
+        if (zpos < 2) {zpos++;break;}
+        else color=nextColor(color);
+        zpos--; break;
+    }
+    drawLed(color,xpos,ypos,zpos);
+    flushBuffer();
+    clearBuffer();
+    delay(animationSpeed);
+  }
+}
+
+void trifade() {
+  continuePattern = true;
+  int animationSpeed = 100;
+  while (continuePattern) {
+    // blue fade out, red fade in
+    for (int i = 1; i <= 8; i++) {
+      drawBox(blue,9-i,0,0,0,2,2,2);
+      drawBox(red,i,0,0,0,2,2,2);
+      flushBuffer();
+      clearBuffer();
+      delay(animationSpeed);
+    }
+    // red fade out, green fade in
+    for (int i = 1; i <= 8; i++) {
+      drawBox(red,9-i,0,0,0,2,2,2);
+      drawBox(green,i,0,0,0,2,2,2);
+      flushBuffer();
+      clearBuffer();
+      delay(animationSpeed);
+    }
+    // green fade out, blue fade in
+    for (int i = 1; i <= 8; i++) {
+      drawBox(green,9-i,0,0,0,2,2,2);
+      drawBox(blue,i,0,0,0,2,2,2);
+      flushBuffer();
+      clearBuffer();
+      delay(animationSpeed);
+    }
+  }
+}
+
+void planarFlop3D() {
+  continuePattern = true;
+  int animationSpeed = 120;
+  while (continuePattern) {
+    
+    for (int y = 2; y>0; y--){
+      for (int z = 0; z < 3; z++) drawLine(color,0,2,z,2,y,z);
+      flushBuffer();
+      clearBuffer();
+      delay(animationSpeed);
+    }
+    for (int x = 2; x > 0; x--) {
+      for (int z = 0; z < 3; z++) drawLine(color,0,2,z,x,0,z);
+      flushBuffer();
+      clearBuffer();
+      delay(animationSpeed);
+    }
+    
+    
+    for (int x = 0; x < 2; x++) {
+      for (int y = 0; y < 3; y++) drawLine(color,0,y,0,x,y,2);
+      flushBuffer();
+      clearBuffer();
+      delay(animationSpeed);
+    }
+    for (int z = 2; z > 0; z--) {
+      for (int y = 0; y < 3; y++) drawLine(color,0,y,0,2,y,z);
+      flushBuffer();
+      clearBuffer();
+      delay(animationSpeed);
+    }
+    
+    for (int z = 0; z < 2; z++) {
+      for (int x = 0; x < 3; x++) drawLine(color,x,0,0,x,2,z);
+      flushBuffer();
+      clearBuffer();
+      delay(animationSpeed);
+    }
+    for (int y = 2; y > 0; y--) {
+      for (int x = 0; x < 3; x++) drawLine(color,x,0,0,x,y,2);
+      flushBuffer();
+      clearBuffer();
+      delay(animationSpeed);
+    }
+    
+    for (int y = 0; y < 2; y++) {
+      for (int z = 0; z < 3; z++) drawLine(color,2,0,z,0,y,z);
+      flushBuffer();
+      clearBuffer();
+      delay(animationSpeed);
+    }
+    for (int x = 0; x < 2; x++) {
+      for (int z = 0; z < 3; z++) drawLine(color,2,0,z,x,2,z);
+      flushBuffer();
+      clearBuffer();
+      delay(animationSpeed);
+    }
+    
+    for (int x = 2; x > 0; x--) {
+      for (int y = 0; y < 3; y++) drawLine(color,2,y,2,x,y,0);
+      flushBuffer();
+      clearBuffer();
+      delay(animationSpeed);
+    }
+    for (int z = 0; z < 2; z++) {
+      for (int y = 0; y < 3; y++) drawLine(color,2,y,2,0,y,z);
+      flushBuffer();
+      clearBuffer();
+      delay(animationSpeed);
+    }
+    
+    for (int z = 2; z > 0; z--) {
+      for (int x = 0; x < 3; x++) drawLine(color,x,2,2,x,0,z);
+      flushBuffer();
+      clearBuffer();
+      delay(animationSpeed);
+    } 
+    for (int y = 0; y < 2; y++) {
+      for (int x = 0; x < 3; x++) drawLine(color,x,2,2,x,y,0);
+      flushBuffer();
+      clearBuffer();
+      delay(animationSpeed);
+    }
+    color = nextColor(color);
+  }
+}
